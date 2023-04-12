a83d522187d12abd30e643e61825fabf355dc387
Rolly Maulana Awangga <rolly@awang.ga>
2023-04-12 14:49:34 +0700

    hmm

--- dipa.go
+++ dipa.go
@@ -4,10 +4,8 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
--- dipa_test.go
+++ dipa_test.go
@@ -7,15 +7,13 @@ import (
 	"github.com/aiteung/atdb"
 )
 
-var MongoConn = atdb. MongoConnect(MongoInfo)
-
 
 func TestInsertlisttamu(t *testing.T) {
 	name := "GABYAZANA"
 	notelp := "(+62)85231151994)"
 	email := "gaby@gmail.com"
 	kota := "BOGOR"
-	hasil := Insertlisttamu(name, notelp, email, kota)
+	hasil := InsertListTamu(name, notelp, email, kota)
 	fmt.Println(hasil)
 }
 
@@ -31,20 +29,20 @@ func TestInsertpesertarapat(t *testing.T) {
 	nama := "Gaby"
 	instansi := "ULBI"
 	status := "Aktif"
-	hasil := Insertpesertarapat(nama, instansi, status)
+	hasil := InsertPesertaRapat(nama, instansi, status)
 	fmt.Println(hasil)
 }
 
 func TestInsertwakturapat(t *testing.T) {
 	hal := "rapat"
 	materi := "Generasi Muda"
-	hasil := Insertwakturapat(hal, materi)
+	hasil := InsertWaktuRapat(hal, materi)
 	fmt.Println(hasil)
 }
 
 func TestInsertrapatmulai(t *testing.T) {
 	pembicara := "Jokowi"
 	durasi := "satu jam"
-	hasil := Insertrapatmulai(pembicara, durasi)
+	hasil := InsertRapatMulai(pembicara, durasi)
 	fmt.Println(hasil)
 }
